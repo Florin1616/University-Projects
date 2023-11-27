0933824c747f4c3ceab6ad1a7efd4e5f275e6c81
Olariu Florin <115114026+Florin1616@users.noreply.github.com>
2023-11-27 22:59:14 +0200

    Add files via upload

---
+++ Semester 1/Computer Systems Architecture/Labs/Lab 5/PB20.asm
@@ -0,0 +1,53 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    a db 2, 1, 3, 3, 4, 2, 6
+    la equ $-a
+    b db 4, 5, 7, 6, 2, 1
+    lb equ $-b
+    r times (la+lb) db 0
+    
+; our code starts here
+segment code use32 class=code
+    start:
+    ;Two byte strings A and B are given. Obtain the string R by 
+    ;   concatenating the elements of B in reverse order and the even elements of A.
+        mov ecx, lb
+        jecxz final1
+        mov edi, 0
+        mov esi, 0
+        add esi, lb-1
+        Repeta1:
+            mov al, [b+esi]
+            mov [r+edi], al
+            inc edi
+            dec esi
+        loop Repeta1
+        final1:
+        
+        mov ecx, la
+        jecxz final2
+        mov esi, 0
+        Repeta2:
+            mov al, [a+esi]
+            test al, 1
+            jnz impar
+            mov [r+edi], al
+            inc edi
+            impar:
+            inc esi
+        loop Repeta2
+        final2:
+        
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Semester 1/Computer Systems Architecture/Labs/Lab 5/PB7.asm
@@ -0,0 +1,49 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    s dd 15345678h, 152B3C4Dh, 0FE98DC76h
+    
+    l1 equ ($-s)/4 ;lungimea in doublewords
+    d times l1 db 0 ;aici as avea o intrebare despre cum trebuie sa declaram d ul deoarece vrem sa stocam octeti
+    trei db 3
+    
+; our code starts here
+segment code use32 class=code
+    start:
+    
+    ;A string of doublewords is given. Obtain the list made out of the high bytes of 
+    ;the high words of each doubleword from the given list with the property that these bytes are multiple of 3.
+        ; ...
+        mov esi, s
+        mov edi, d
+        cld
+        mov ecx, l1
+        jecxz final
+        repeta: 
+            lodsb ; in al avem byte ul low din word ul low
+            lodsb ; in al avem byte ul high din word ul low
+            lodsb 
+            lodsb ; in al avem byte ul high din word ul high
+            mov dl, al
+            mov ah, 0 ;il facem word ca sa putem vedea daca este divizibil cu 3
+            div byte[trei]
+            cmp ah, 0
+            jnz nondivizibil
+                mov al, dl
+                stosb
+            nondivizibil:
+        loop repeta
+    
+        ; exit(0)
+        final:
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
